eec2b6fd34b6cca1009aedb8a8834834c9d64d43
franklinRalali <franklin.tamboto@ralali.com>
2023-08-13 23:46:00 +0700

    fix: make random generator to be in singleton with interface, so that it can be testable with unit testing

--- pkg/rands/rands.go
+++ pkg/rands/rands.go
@@ -3,26 +3,51 @@ package rands
 import (
 	"crypto/rand"
 	"errors"
+	"io"
 	"math/big"
 	"strings"
 )
 
-const letterBytes = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+type Randoms interface {
+	String(n int, charSet string) (string, error)
+}
 
-func RandString(n int) (string, error) {
+type randoms struct {
+	source io.Reader
+}
+
+const (
+	DefaultCharSet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+)
+
+var (
+	DefaultSource = rand.Reader
+)
+
+func New(source io.Reader) Randoms {
+	if source == nil {
+		source = rand.Reader
+	}
+
+	return randoms{
+		source: source,
+	}
+}
+
+func (rs randoms) String(n int, charSet string) (string, error) {
 	if n <= 0 {
 		return "", errors.New("length must be greater than 0")
 	}
 
-	letterLen := len(letterBytes)
+	letterLen := len(charSet)
 	var strBuilder strings.Builder
 	for i := 0; i < n; i++ {
-		num, err := rand.Int(rand.Reader, big.NewInt(int64(letterLen-1)))
+		num, err := rand.Int(rs.source, big.NewInt(int64(letterLen-1)))
 		if err != nil {
 			return "", err
 		}
 
-		strBuilder.WriteByte(letterBytes[num.Int64()])
+		strBuilder.WriteByte(charSet[num.Int64()])
 	}
 
 	return strBuilder.String(), nil
